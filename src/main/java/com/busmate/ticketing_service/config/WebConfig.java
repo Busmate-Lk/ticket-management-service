5cbb4e54726d4d29399c26189752c6d249172673
ILakshitha <ilakshitha7921@gmail.com>
2025-10-18 02:44:01 +0530

    Rename CorsConfig to WebConfig and update CORS configuration to allow specific origins

---
+++ src/main/java/com/busmate/ticketing_service/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.busmate.ticketing_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://busmate-web-frontend.vercel.app"
+                    ) // list each frontend separately
+                    // .allowedOrigins("*") // Tempory CORS allowance for all URLs
+                    .allowedMethods("*") // GET, POST, PUT, etc.
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
